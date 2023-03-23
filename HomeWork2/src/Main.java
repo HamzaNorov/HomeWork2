54b98de11ca6e4bd457be04c46675cdb0d7ead19
HamzaNorov <bahtiernorov@gmail.com>
2023-03-24 00:23:59 +0300

    счетовод для купля продажи

--- HomeWork2/src/Main.java
+++ HomeWork2/src/Main.java
@@ -2,6 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите количество товара на складе: ");
+        int stock = input.nextInt();
+        System.out.print("Введите количество проданного товара: ");
+        int sold = input.nextInt();
+
+        int remainder = stock - sold;
+        System.out.println("Осталось товара на складе: " + remainder);
+
+        System.out.print("Введите оптовую цену одного товара: ");
+        double wholesale = input.nextDouble();
+
+        double acquired = stock * wholesale;
+        System.out.println("Цена закупа товара: " + acquired);
+
+        System.out.print("Введите стоимость продажи одного товара: ");
+        double price = input.nextDouble();
+
+        double revenue = sold * price;
+        System.out.println("Доход от продаж: " + revenue);
+    }
+}
+/*public class Main {
+    public static void main(String[] args) {
         System.out.println("\t\t\t***Сколько вам лет***\n");
         int age = new Scanner(System.in).nextInt();
         if (age >= 18) {
@@ -10,4 +35,4 @@ public class Main {
             System.out.println("\t\t\t***Too Young***\n");
         }
     }
-}
+}**/
